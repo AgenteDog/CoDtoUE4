1b91df8483eded9a9fa088f2d654e989a47d7162
Agente Dog <54131016+AgenteDog@users.noreply.github.com>
2020-05-01 02:37:59 -0400

    Improved
    Improved placing so that it is much more accurate and true to the game.

--- Placing.py
+++ Placing.py
@@ -5,7 +5,7 @@ from unreal_engine.classes import StaticMeshActor, StaticMesh
 import json, os, glob, math
 
 geo_map = "mp_hijacked"
-geo_directory = r"E:\Bored\hijacked"
+geo_directory = r"E:\Stufff\Hijacked"
 xmodels_path = r"/Game/CoD/"
 extension = ".tga"
 
@@ -20,17 +20,11 @@ xmodels_json = geo_directory + "\\" + geo_map + "\\" + geo_map + "_xmodels.json"
 with open(xmodels_json, 'r') as file:
     entities = json.load(file)
 
-cube_placeholder = ue.load_object(StaticMesh, '/Engine/BasicShapes/Cube.Cube')
-cube = ue.get_editor_world().actor_spawn(StaticMeshActor, FVector(0, 0, 0), FRotator(0, 0, 0))
-cube.StaticMeshComponent.StaticMesh = cube_placeholder
-cube.set_actor_label('map_cube')
-
 mesh_number = 0
 last_mesh = ''
+
 try:
     for actor in entities:
-        if not actor.get('Name'):
-            continue
 
         #Set all the coordinates, scale and rotations.
         RotX = (float(actor['RotX'])) * math.pi/180
@@ -41,16 +35,15 @@ try:
         RotY = RotY * 180/math.pi
         RotZ = RotZ * 180/math.pi
 
-        PosX = float(actor['PosX']) * 2.54
-        PosY = float(actor['PosY']) * 2.54
-        PosZ = float(actor['PosZ']) * 2.54
+        PosX = float(actor['PosX'])
+        PosY = float(actor['PosY'])
+        PosZ = float(actor['PosZ'])
 
-        Scale = float(actor['Scale'])
+        Scale = float(actor['Scale']) * 0.394
 
         #Place the mesh
         mesh = ue.load_object(StaticMesh, xmodels_path + 'xmodels' + '/' + actor['Name'] + '.' + actor['Name'])
         obj = ue.get_editor_world().actor_spawn(StaticMeshActor)
-        obj.attach_to_actor(cube)
         obj.set_actor_scale(Scale, -Scale, Scale)
         obj.set_actor_location(PosX, PosY, PosZ)
         obj.set_actor_rotation(-RotX, -RotY, RotZ)
@@ -69,10 +62,10 @@ try:
             obj.set_actor_label(actor['Name'] + ' ' + str(mesh_number))
 
 except:
-    cube.set_actor_scale(1, -1, 1)
-
+    print("Done or exception!")
 
 mesh = ue.load_object(StaticMesh, xmodels_path + 'MapGeo' + '/' + geo_map + '.' + geo_map)
 obj = ue.get_editor_world().actor_spawn(StaticMeshActor)
+obj.set_actor_scale( 0.394, -0.394, 0.394 );
 obj.StaticMeshComponent.StaticMesh = mesh
 obj.set_actor_label("Map Geometry")
